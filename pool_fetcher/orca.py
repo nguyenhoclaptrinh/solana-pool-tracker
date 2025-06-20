230d5c86db5a22e5725fc7bd7c4ee071226fe9c3
Nguyen Truong <thainguyentruongpl2@gmail.com>
2025-06-20 20:26:33 +0700

    Done Orca API

--- pool_fetcher/orca.py
+++ pool_fetcher/orca.py
@@ -1,33 +1,43 @@
 # pool_fetcher/orca.py
-import requests
 import json
+import requests
+
+
+# CONSTAINS
+ORCA_API_URL = "https://api.orca.so/v2/solana/pools"
 
 
 def get_orca_pools(token_list):
     try:
-        url = "https://api.orca.so/v2/solana/pools"
-        res = requests.get(url)
+        res = requests.get(ORCA_API_URL)
         pools = res.json().get("data", [])
 
-        # Ghi kết quả vào file orca.json
-        with open("orca.json", "w", encoding="utf-8") as f:
-            json.dump(pools, f, ensure_ascii=False, indent=2)
-        # print(pools)
         result = []
         for pool in pools:
             mint_a = pool.get("tokenMintA")
             mint_b = pool.get("tokenMintB")
 
             if mint_a in token_list or mint_b in token_list:
-                token = pool['tokenA']['symbol'] if mint_a in token_list else pool['tokenB']['symbol']
-                result.append({
+                item = {
                     "dex": "Orca",
-                    "tokenA": tokenA,
-                    "tokenB": tokenB,
+                    "tokenA": {
+                        "symbol": pool["tokenA"]["symbol"],
+                        "mint": pool["tokenMintA"]
+                    },
+                    "tokenB": {
+                        "symbol": pool["tokenB"]["symbol"],
+                        "mint": pool["tokenMintB"]
+                    },
+                    "pool_address": pool["address"],
                     "price": float(pool.get("price", 0)),
-                    "volume": float(pool.get("stats", {}).get("ANY_ADDITIONAL_PROPERTY", {}).get("volume", 0))
-                    "pool_address": pool.get("address"),
-                })
+                    "volume_24h": float(pool.get("stats", {}).get("24h", {}).get("volume", 0)),
+                    "liquidity_usd": float(pool.get("tvlUsdc", 0))
+                }
+                result.append(item)
+
+        # Ghi kết quả vào file orca.json
+        # with open("orca_test.json", "w", encoding="utf-8") as f:
+        #     json.dump(result, f, ensure_ascii=False, indent=2)
 
         return result
     except Exception as e:
@@ -35,7 +45,8 @@ def get_orca_pools(token_list):
         return []
 
 
-if __name__ == '__main__':
-    print("find pool SOL")
-    tokens = ["So11111111111111111111111111111111111111112"]
-    result = get_orca_pools(tokens)
+# if __name__ == '__main__':
+#     print("Find pool SOL")
+#     tokens = ["So11111111111111111111111111111111111111112"]
+#     result = get_orca_pools(tokens)
+#     print("Done")
